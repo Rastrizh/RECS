39e75d52cd17f7fab150fa155da957ba7c81ae0f
Александр Растриженков <a.rastrizhenkov@gmail.com>
2020-04-23 21:47:59 +0300

    Components reworked, added static component container class

--- RECS/src/Components.h
+++ RECS/src/Components.h
@@ -3,42 +3,59 @@
 
 #include "Entities.h"
 #include <unordered_map>
+#include <typeinfo>
 
 namespace RECS {
-	template<typename T>
 	class IComponent
 	{
-	private:
-		class ComponentContainer
-		{
-		private:
-			std::unordered_map<EntityID, IComponent<T>*> container;
-		public:
-			void push(EntityID entityID, IComponent<T>* component);
-			void pop(EntityID entityID);
-		};
-	private:
-		ComponentContainer container;
 	public:
-		virtual void AddComponent(EntityID entityID)
-		{
-			container.push(entityID, this);
-		}
-		virtual void DeleteComponent(EntityID entityID)
-		{
-			container.pop(entityID);
-		}
+		virtual ~IComponent() = default;
 	};
-	template<typename T>
-	inline void IComponent<T>::ComponentContainer::push(EntityID entityID, IComponent<T>* component)
+
+	template<class T>
+	class Component : public IComponent
 	{
-		container.emplace(entityID, component);
+	public:
+		~Component() override = default;
+	private:
+		static const size_t STATIC_COMONENT_TYPE_ID;
+	public:
+		static const size_t GetTypeID() { return STATIC_COMONENT_TYPE_ID; }
+	};
+	template<class T>
+	const size_t Component<T>::STATIC_COMONENT_TYPE_ID = typeid(T).hash_code();
+
+	class ComponentContainer
+	{
+	public:
+		static std::unordered_map<size_t, std::unordered_map<EntityID, IComponent*>> container;
+	public:
+		template<class T, class ... P>
+		static void AddComponent(EntityID ownerId, P&&... params);
+		
+		template<class T>
+		static void DeleteComponent(EntityID ownerId);
+
+		template<class T>
+		static T* GetComponent(EntityID ownerId);
+	};
+	std::unordered_map<size_t, std::unordered_map<EntityID, IComponent*>> ComponentContainer::container;
+
+	template<class T, class ...P>
+	inline void ComponentContainer::AddComponent(EntityID ownerId, P && ...params)
+	{
+		IComponent *component = new T(std::forward<P>(params) ...);
+		container[T::GetTypeID()][ownerId] = component;
 	}
-	template<typename T>
-	inline void IComponent<T>::ComponentContainer::pop(EntityID entityID)
+	template<class T>
+	inline void ComponentContainer::DeleteComponent(EntityID ownerId)
 	{
-		container.erase(entityID);
+		container[T::GetTypeID()].erase(ownerId);
+	}
+	template<class T>
+	inline T * ComponentContainer::GetComponent(EntityID ownerId)
+	{
+		return dynamic_cast<T*>(container[T::GetTypeID()][ownerId]);
 	}
 }
-
 #endif // !COMPONENTS_H
