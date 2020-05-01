e1240f769bfad25de6b11936b9f24a60c92c419b
Александр Растриженков <a.rastrizhenkov@gmail.com>
2020-05-01 12:29:21 +0300

    Add logic to get targets of system

--- RECS/src/Systems.h
+++ RECS/src/Systems.h
@@ -2,7 +2,7 @@
 #define SYSTEMS_H
 
 #include <vector>
-#include <functional>
+#include <algorithm>
 #include "Components.h"
 
 #define EXPAND(...) __VA_ARGS__
@@ -16,19 +16,46 @@
 #define FOR_EACH(MODIFIER, ...) EXPAND(GET_MACRO(__VA_ARGS__, FUNC_6, FUNC_5, FUNC_4, FUNC_3, FUNC_2, FUNC_1)(MODIFIER, __VA_ARGS__))
 
 #define COMPONENT_GET_TYPE_ID(COMPONENT_CLASS) COMPONENT_CLASS::GetTypeID()
-#define CREATE_COMPONENT_TYPE_ID_LIST(...) { FOR_EACH(COMPONENT_GET_TYPE_ID, __VA_ARGS__) }
+#define CREATE_TARGET_ENTITY_ID_LIST(...) (std::vector<RECS::EntityID>)(RECS::ISystem::GetTargetEntityIDs(std::vector<size_t>({ FOR_EACH(COMPONENT_GET_TYPE_ID, __VA_ARGS__) })))
 
 namespace RECS {
 	class ISystem
 	{
-	private:
-		std::vector<IComponent*> m_targets;
+	protected:
+		std::vector<IEntity*> m_targets;
 	public:
-		void GetTargets() 
-		{
-			
-		}
+		static std::vector<EntityID> GetTargetEntityIDs(std::vector<size_t>);
+		//std::vector<IEntity*> GetTargetEntities(std::vector<EntityID>);
+		//void GetTargets(std::initializer_list<EntityID>) { }
 	};
+
+	std::vector<EntityID> ISystem::GetTargetEntityIDs(std::vector<size_t> iList)
+	{
+		std::vector<EntityID> t_iList;
+		for (auto &p : iList)
+		{
+			for (auto &b : ComponentContainer::container[p])
+				t_iList.push_back(b.first);
+		}
+
+		std::sort(t_iList.begin(), t_iList.end());
+		for (auto p = t_iList.begin(); p!=t_iList.end()-1; ++p)
+		{
+			if (*p != *(std::next(p,1))) {
+				if (p == t_iList.begin())
+				{
+					t_iList.erase(t_iList.begin());
+					p = t_iList.begin();
+					continue;
+				}
+				t_iList.erase(std::next(p));
+			}
+		}
+		auto last = std::unique(t_iList.begin(), t_iList.end());
+		t_iList.erase(last, t_iList.end());
+
+		return t_iList;
+	}
 }
 
 #endif // !SYSTEMS_H
