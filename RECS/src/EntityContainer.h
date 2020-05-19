fa76705898fc459ae282bc241cb2f7db2448a70d
rastrizh <b.orhidna@gmail.com>
2020-05-19 16:49:43 +0300

    Move macroses for component types to entity container

--- RECS/src/EntityContainer.h
+++ RECS/src/EntityContainer.h
@@ -6,6 +6,19 @@
 #include <unordered_map>
 #include <vector>
 
+#define EXPAND(...) __VA_ARGS__
+#define FUNC_1(MODIFIER, X) MODIFIER(X)
+#define FUNC_2(MODIFIER, X, ...) MODIFIER(X), FUNC_1(MODIFIER, __VA_ARGS__)
+#define FUNC_3(MODIFIER, X, ...) MODIFIER(X), FUNC_2(MODIFIER, __VA_ARGS__)
+#define FUNC_4(MODIFIER, X, ...) MODIFIER(X), FUNC_3(MODIFIER, __VA_ARGS__)
+#define FUNC_5(MODIFIER, X, ...) MODIFIER(X), FUNC_4(MODIFIER, __VA_ARGS__)
+#define FUNC_6(MODIFIER, X, ...) MODIFIER(X), FUNC_5(MODIFIER, __VA_ARGS__)
+#define GET_MACRO(_1, _2, _3, _4, _5, _6, NAME, ...) NAME
+#define FOR_EACH(MODIFIER, ...) EXPAND(GET_MACRO(__VA_ARGS__, FUNC_6, FUNC_5, FUNC_4, FUNC_3, FUNC_2, FUNC_1)(MODIFIER, __VA_ARGS__))
+
+#define COMPONENT_GET_TYPE_ID(COMPONENT_CLASS) COMPONENT_CLASS::GetTypeID()
+#define CREATE_TARGET_ENTITY_ID_LIST(...) (std::list<RECS::ComponentType>({ FOR_EACH(COMPONENT_GET_TYPE_ID, __VA_ARGS__) }))
+
 namespace RECS {
 	class EntityContainer
 	{
--- RECS/src/Systems.h
+++ RECS/src/Systems.h
@@ -5,25 +5,13 @@
 #include <algorithm>
 #include "Entities.h"
 #include "Components.h"
-
-#define EXPAND(...) __VA_ARGS__
-#define FUNC_1(MODIFIER, X) MODIFIER(X)
-#define FUNC_2(MODIFIER, X, ...) MODIFIER(X), FUNC_1(MODIFIER, __VA_ARGS__)
-#define FUNC_3(MODIFIER, X, ...) MODIFIER(X), FUNC_2(MODIFIER, __VA_ARGS__)
-#define FUNC_4(MODIFIER, X, ...) MODIFIER(X), FUNC_3(MODIFIER, __VA_ARGS__)
-#define FUNC_5(MODIFIER, X, ...) MODIFIER(X), FUNC_4(MODIFIER, __VA_ARGS__)
-#define FUNC_6(MODIFIER, X, ...) MODIFIER(X), FUNC_5(MODIFIER, __VA_ARGS__)
-#define GET_MACRO(_1, _2, _3, _4, _5, _6, NAME, ...) NAME
-#define FOR_EACH(MODIFIER, ...) EXPAND(GET_MACRO(__VA_ARGS__, FUNC_6, FUNC_5, FUNC_4, FUNC_3, FUNC_2, FUNC_1)(MODIFIER, __VA_ARGS__))
-
-#define COMPONENT_GET_TYPE_ID(COMPONENT_CLASS) COMPONENT_CLASS::GetTypeID()
-#define CREATE_TARGET_ENTITY_ID_LIST(...) (std::list<RECS::ComponentType>({ FOR_EACH(COMPONENT_GET_TYPE_ID, __VA_ARGS__) }))
+#include "Groups.h"
 
 namespace RECS {
 	class ISystem
 	{
 	protected:
-		std::vector<Entity*> m_targets;
+		Group* m_targetGroup;
 	public:
 		virtual ~ISystem() = default;
 		virtual void CreatePool() = 0;
