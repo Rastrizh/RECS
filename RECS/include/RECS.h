170b075c1846050412584d1badb51a06c4cd259c
Rastrizh <a.rastrizhenkov@gmail.com>
2021-06-01 19:21:03 +0300

    update project files

--- RECS/include/RECS.h
+++ RECS/include/RECS.h
@@ -1,15 +1,8 @@
 #ifndef RECS_H
 #define RECS_H
 
-#include "../src/Entities.h"
-#include "../src/Groups.h"
-#include "../src/Events/Delegate.h"
-#include "../src/Events/Event.h"
-#include "../src/Components.h"
 #include "../src/Systems.h"
-#include "../src/EntityContainer.h"
-#include "../src/ComponentContainer.h"
-#include "../src/Engine.h"
-#include "../src/Timer.h"
+#include "../src/Register.h"
+#include "../src/core/Timer.h"
 
 #endif
