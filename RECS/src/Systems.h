6e7de20639daaf22347b6ccd642a520e1ae295ae
Rastrizh <a.rastrizhenkov@gmail.com>
2021-06-01 19:15:01 +0300

    Change includes and delete group

--- RECS/src/Systems.h
+++ RECS/src/Systems.h
@@ -1,18 +1,14 @@
 #ifndef SYSTEMS_H
 #define SYSTEMS_H
 
-#include <vector>
-#include <algorithm>
-#include "Entities.h"
-#include "Components.h"
-#include "Groups.h"
-#include "Timer.h"
+#include <set>
+#include "RECSTypes.h"
 
 namespace RECS {
 	class ISystem
 	{
 	protected:
-		Group* m_targetGroup;
+		std::set<entityID> m_targetGroup;
 	public:
 		float m_deltaTime = 1.F / 61.F;
 	public:
