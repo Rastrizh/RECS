4e6eb039b986cb64269027ffcb549db8c11ad0c4
Александр Растриженков <a.rastrizhenkov@gmail.com>
2021-06-04 16:42:52 +0300

    remove entity table

--- RECS/src/EntityManager.h
+++ RECS/src/EntityManager.h
@@ -12,30 +12,30 @@ namespace RECS {
 	{
 	private:
 		static memory::ChunkAllocator<Entity> s_entityManager_allocator;
-		static std::map<entityID, Entity*> s_entity_lookup_table;
+		//static std::map<entityID, Entity*> s_entity_lookup_table;
 	public:
 		static Entity* CreateEntity()
 		{
 			//RINFO("Entity manager");
 			Entity* e = new(s_entityManager_allocator.alloc(sizeof(Entity))) Entity();
-			s_entity_lookup_table[e->EntityID] = e;
+			//s_entity_lookup_table[e->EntityID] = e;
 			return e;
 		}
-		static bool IsUpdateble(const entityID& eid)
+		static bool IsUpdateble(entityID eid)
 		{
-			return s_entity_lookup_table[eid]->isUpdateble;
+			return s_entityManager_allocator[eid]->isUpdateble;
 		}
-		static Entity* getEntityPtr(const entityID& eid)
+		//static Entity* getEntityPtr(const entityID& eid)
+		//{
+		//	return s_entity_lookup_table[eid];
+		//}
+		static void DeleteEntity(entityID eid)
 		{
-			return s_entity_lookup_table[eid];
-		}
-		static void DeleteEntity(const entityID& e)
-		{
-			s_entityManager_allocator.dealloc(s_entity_lookup_table[e]);
+			s_entityManager_allocator.dealloc(s_entityManager_allocator[eid]);
 		}
 	};
 
-	std::map<entityID, Entity*> EntityManager::s_entity_lookup_table;
+	//std::map<entityID, Entity*> EntityManager::s_entity_lookup_table;
 
 	memory::ChunkAllocator<Entity> EntityManager::s_entityManager_allocator{
 		memory::MemoryManager::NewMemoryUser(typeid(EntityManager).name(), sizeof(Entity) * MAX_ENTITY_COUNT),
