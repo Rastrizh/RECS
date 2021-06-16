863d189f5162c6f2b1f545d6405bf90ad999e559
Александр Растриженков <a.rastrizhenkov@gmail.com>
2021-06-16 08:50:12 +0300

    Clean from old code

--- RECS/src/Register.h
+++ RECS/src/Register.h
@@ -26,22 +26,11 @@ class Engine
 {
 public:
 	static std::mutex s_Engine_lock;
-	//static std::map<entityID, std::map<ComponentTypeID, IComponent*>> s_entity_components;
-	//static std::map<ComponentTypeID, std::set<entityID>> s_entity_table;
 
 	static event<Entity*> OnEntityCreated;
 	static event<Entity*> OnEntityDestroyed;
 
 public:
-	static void Initialize()
-	{
-		//Engine::OnEntityDestroyed += Engine::DeleteEntity;
-		//Engine::OnEntityDestroyed += EntityManager::DeleteEntity;
-
-		ComponentManager::OnComponentAdded += Engine::ComponentAdded;
-
-		ComponentManager::OnComponentRemoved += Engine::ComponentRemoved;
-	}
 
 	static Entity* CreateEntity()
 	{
@@ -50,8 +39,6 @@ public:
 
 	static void KillEntity(Entity* e)
 	{
-		//std::lock_guard<std::mutex> Lock(s_Engine_lock);
-
 		ComponentManager::DeleteEntityComponents(e);
 		EntityManager::DeleteEntity(e);
 		OnEntityDestroyed(e);
@@ -61,11 +48,6 @@ public:
 		EntityManager::DeleteAll();
 		ComponentManager::Clear();
 	}
-
-	//static void DeleteEntity(const entityID& eid)
-	//{
-	//	ComponentManager::DeleteEntity(s_entity_components[eid]);
-	//}
 
 	template<typename ...Types>
 	static void each(std::common_type_t<std::function<void(Entity*, ComponentHandle<Types>...)>> view)
@@ -80,56 +62,9 @@ public:
 				continue;
 		}
 	}
-
-	//template<class ... Args>
-	//static std::set<entityID> getGroup()
-	//{
-	//	//std::lock_guard<std::mutex> Lock(s_Engine_lock);
-	//	std::vector<ComponentTypeID> v{ (Args::GetTypeID())... };
-	//	auto temp = s_entity_table[v[0]];
-	//	if (v.size() < 2)
-	//		return temp;
-	//	std::set<entityID> target;
-	//	for (size_t i = 1; i < v.size(); i++)
-	//	{
-	//		target = s_entity_table[v[i]];
-	//		temp.merge(target);
-	//		temp = target;
-	//	}
-	//	return target;
-	//}
-
-	//template<class T>
-	//static T* getComponent(const entityID& eid)
-	//{
-	//	//std::lock_guard<std::mutex> Lock(s_Engine_lock);
-	//	return (T*)s_entity_components[eid][T::GetTypeID()];
-	//}
-	//template<class T>
-	//static bool hasComponent(const entityID& eid)
-	//{
-	//	std::lock_guard<std::mutex> Lock(s_Engine_lock);
-	//	return !s_entity_components[eid].empty();
-	//}
-	static void ComponentAdded(const entityID& eid, const ComponentTypeID& componentType, IComponent* component)
-	{
-		std::lock_guard<std::mutex> Lock(s_Engine_lock);
-		//s_entity_components[eid][componentType] = component;
-		//s_entity_table[componentType].insert(eid);
-	}
-	static void ComponentRemoved(const entityID& eid, const ComponentTypeID& componentType)
-	{
-		std::lock_guard<std::mutex> Lock(s_Engine_lock);
-		//s_entity_table.erase(componentType);
-		//s_entity_components[eid].erase(componentType);
-	}
 };
 
 std::mutex Engine::s_Engine_lock;
-
-//std::map<entityID, std::map<ComponentTypeID, IComponent*>> Engine::s_entity_components;
-
-//std::map<ComponentTypeID, std::set<entityID>> Engine::s_entity_table;
 
 event<Entity*> Engine::OnEntityCreated;
 event<Entity*> Engine::OnEntityDestroyed;
